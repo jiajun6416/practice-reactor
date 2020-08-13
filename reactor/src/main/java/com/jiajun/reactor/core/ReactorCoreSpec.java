d98f75809a76c6e9d97478ee640faf7a2da76f80
Jiajun Liao <jiajun6416@gmail.com>
2020-08-13 09:32:22 +0800

    reactor2

--- reactor/src/main/java/com/jiajun/reactor/core/ReactorCoreSpec.java
+++ reactor/src/main/java/com/jiajun/reactor/core/ReactorCoreSpec.java
@@ -6,18 +6,28 @@ import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.SettableFuture;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.junit.Test;
+import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscription;
 import reactor.core.Disposable;
+import reactor.core.Exceptions;
 import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
@@ -324,11 +334,228 @@ public class ReactorCoreSpec {
      * 线程模型
      */
     @Test
-    public void threadModel() {
+    public void threadModel() throws InterruptedException {
         // single 线程
         Flux.interval(Duration.ofSeconds(1), Schedulers.newSingle("interval", false)).subscribe(System.out::println);
 
-        // publishOn subscribeOn
+        // publishOn: 指定之后操作使用的线程
+        Flux<String> flux = Flux.range(10, 10)
+                .map(i -> {
+                    System.out.println("map1 thread: " + Thread.currentThread().getName());
+                    return 10 + i;
+                })
+                .publishOn(Schedulers.newParallel("custom-executor"))
+                .map(i -> {
+                    System.out.println("map2 thread: " + Thread.currentThread().getName());
+                    return "value: " + i;
+                });
+        //new Thread((flux::subscribe)).start();
+
+        // subscribeOn: 指定subscriber的线程, 但是只有subscriber才会触发publisher的计算, 所以操作线程一般都是subscriber线程, 此时会同时被指定.
+        // subscribeOn: 和放置顺序无关
+        Flux<String> flux2 = Flux.range(10, 10)
+                .map(i -> {
+                    System.out.println("map1 thread: " + Thread.currentThread().getName());
+                    return 10 + i;
+                })
+                .subscribeOn(Schedulers.newParallel("custom-executor"))
+                .map(i -> {
+                    System.out.println("map2 thread: " + Thread.currentThread().getName());
+                    return "value: " + i;
+                });
+        // 无论在什么线程中订阅的, 都会转移到subscribeOn指定的线程
+        new Thread((flux2::subscribe)).start();
+
+        TimeUnit.SECONDS.sleep(1);
     }
 
+    /**
+     * 错误处理: 只要发生错误,序列都是终止的.
+     * 1. catch 做fallback,
+     * - 返回其他值
+     * - 执行异常方法
+     * 2. 包装成业务异常
+     * 3. finally中进行资源清理
+     */
+    @Test
+    public void errorHandler() throws InterruptedException {
+        Function<Integer, Integer> errorFunction = value -> value / (value - 5);
+
+        // subscriber中onError, 此时订阅结束
+        Flux.range(1, 10).map(errorFunction).subscribe(System.out::println, error -> System.out.println("catch error: " + error.getMessage()));
+        System.out.println("--------------------------------");
+        // onErrorReturn: 订阅同样会结束掉
+        Flux.range(1, 10).map(errorFunction).onErrorReturn(110).subscribe(System.out::println, error -> System.out.println("catch error: " + error.getMessage()));
+        System.out.println("--------------------------------");
+        Flux.range(1, 10).map(errorFunction).onErrorReturn(e -> e instanceof ArithmeticException, 110).subscribe(System.out::println, error -> System.out.println("catch error: " + error.getClass().getName()));
+        // onErrorResume: 订阅同样会结束掉
+        System.out.println("--------------------------------");
+        Flux.range(1, 10).map(errorFunction).onErrorResume(e -> Mono.just(110)).subscribe(System.out::println, error -> System.out.println("catch error: " + error.getMessage()));
+
+        // 转换异常: 通过构建一个error的publisher 或 onErrorMap
+        System.out.println("--------------------------------");
+        Flux.range(1, 10).map(errorFunction).onErrorResume(e -> Mono.error(new RuntimeException(e))).subscribe(System.out::println, error -> System.out.println("catch error: " + error.getMessage())); // 通过map
+        Flux.range(1, 10).map(errorFunction).onErrorMap(RuntimeException::new).subscribe(System.out::println, error -> System.out.println("catch error: " + error.getMessage()));
+
+        // doOnXXX: 仅仅回调, 不会带来任何改动(一般用作监控)
+        Flux.range(1, 10).map(errorFunction).doOnError(e -> System.out.println("do on error. " + e.getMessage())).subscribe(System.out::println);
+
+        System.out.println("--------------------------------");
+        // using: try-with-resource, 用作资源回收
+        Flux.using(
+                () -> new BufferedReader(new InputStreamReader(new FileInputStream("/script/tbj"))),
+                bufferedReader -> Flux.generate(sink -> {
+                    String line = null;
+                    try {
+                        line = bufferedReader.readLine();
+                    } catch (IOException e) {
+                    }
+                    if (line != null) {
+                        sink.next(line);
+                    } else {
+                        sink.complete();
+                    }
+                }),
+                bufferedReader -> {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                    }
+                }
+        ).subscribe(System.out::println);
+        System.out.println("--------------------------------");
+        // doFinally: 只要订阅终止即会触发, 包括complete error
+        Flux.range(1, 2).doFinally(type -> System.out.println("doFinally: " + type)).subscribe();
+        Flux.range(1, 2).doFinally(type -> System.out.println("doFinally: " + type)).take(1).subscribe();
+        Flux.range(1, 10).map(errorFunction).doFinally(type -> System.out.println("doFinally: " + type)).subscribe(integer -> {
+        }, throwable -> {
+        });
+
+        // retry: 重订阅, 会再从头开始处理, retry(n)表示重订阅n次, 如果n次后还失败再丢出异常
+        System.out.println("--------------------------------");
+        Flux.interval(Duration.ofMillis(250))
+                .map(input -> {
+                    if (input < 3) return "tick " + input;
+                    throw new RuntimeException("boom");
+                })
+                .retry(1)
+                .subscribe(System.out::println, System.err::println);
+
+        Thread.sleep(2100);
+
+        // Exceptions: 对异常进行包装后传递
+        System.out.println("--------------------------------");
+        Flux.range(1, 10).map(i -> {
+            try {
+                if (i > 4) {
+                    throw new IOException("too large");
+                }
+                return i;
+            } catch (IOException e) {
+                throw Exceptions.propagate(e);
+            }
+        }).subscribe(System.out::println, e -> System.out.println(Exceptions.unwrap(e)));
+    }
+
+    /**
+     * transform: 将操作链上动作外置
+     */
+    @Test
+    public void transform() {
+        // transform: 等同于在链上直接写操作符
+        Function<Flux<String>, Publisher<String>> filterAndMap = str -> str.filter(color -> !color.equals("blue")).map(String::toUpperCase);
+        Flux.just("blue", "green", "orange", "purple").transform(filterAndMap).subscribe(System.out::println);
+        Flux.just("blue", "green", "orange", "purple").filter(color -> !color.equals("blue")).map(String::toUpperCase).subscribe(System.out::println);
+
+        // transformDeferred: 延时执行的transform, 每次订阅过程都会重新再次构建链, 可以实现多次订阅的处理链不同
+        // defer概念: 利用延迟将cold -> hot
+        System.out.println("--------------------------------");
+        AtomicInteger ai = new AtomicInteger();
+        filterAndMap = str -> {
+            if (ai.incrementAndGet() == 1) {
+                return str.filter(color -> !color.equals("orange")).map(String::toLowerCase);
+            }
+            return str.filter(color -> !color.equals("purple")).map(String::toUpperCase);
+        };
+        Flux<String> composeFlux = Flux.just("blue", "green", "orange", "purple").transformDeferred(filterAndMap);
+        composeFlux.subscribe(System.out::println);
+        composeFlux.subscribe(System.out::println);
+
+        // windowing: 可以用来实现metric
+        System.out.println("--------------------------------");
+        Flux.range(1, 10).window(5).concatMap(integerFlux -> Mono.just(integerFlux.toStream().map(String::valueOf).collect(Collectors.joining(",")))).subscribe(System.out::println);
+        // buffer
+    }
+
+    /**
+     * groupBy返回: Flux<GroupedFlux<K, T>>
+     */
+    @Test
+    public void groupBy() {
+        StepVerifier.create(Flux.just(1, 3, 5, 2, 4, 6, 11, 12, 13)
+                .groupBy(i -> i % 2 == 0 ? "even" : "odd")
+                .concatMap(g -> g.defaultIfEmpty(-1) //如果组为空，显示为 -1
+                        .map(String::valueOf) //转换为字符串
+                        .startWith(g.key())) //以该组的 key 开头
+        )
+                .expectNext("odd", "1", "3", "5", "11", "13")
+                .expectNext("even", "2", "4", "6", "12")
+                .verifyComplete();
+    }
+
+    /**
+     * window返回Flux<Flux<T>>
+     */
+    @Test
+    public void windowing() {
+        // window(maxSize): 收集5个元素关闭窗口, 开启下一个窗口.
+        StepVerifier.create(Flux.range(1, 10)
+                .window(5)
+                .concatMap(g -> g.defaultIfEmpty(-1)) //windows为空 显示为 -1
+        )
+                .expectNext(1, 2, 3, 4, 5)
+                .expectNext(6, 7, 8, 9, 10)
+                .verifyComplete();
+
+        // window(int maxSize, int skip)
+        StepVerifier.create(Flux.range(1, 10)
+                .window(5, 3) // 收集5个元素关闭窗口, 收集3个元素开启窗口. 会层叠2个元素, 如果maxSize>skip则会有元素不在窗口中
+                .concatMap(g -> g.defaultIfEmpty(-1)) //windows为空 显示为 -1
+        )
+                .expectNext(1, 2, 3, 4, 5)
+                .expectNext(4, 5, 6, 7, 8)
+                .expectNext(7, 8, 9, 10)
+                .expectNext(10)
+                .verifyComplete();
+    }
+
+    /**
+     * 指定默认的执行器
+     * 修改默认Scheduler为自定义鲜橙汁, 在需要使用额外线程的场景无需自己指定
+     */
+    @Test
+    public void settingDefaultScheduler() {
+        // Schedulers中内置了三种常用线程模型, 在不同操作总如果不指定,或者使用以下方式则共用同一个线程池
+        Schedulers.setFactory(new Schedulers.Factory() {
+            // 修改默认的boundElastic实现
+            @Override
+            public Scheduler newBoundedElastic(int threadCap, int queuedTaskCap, ThreadFactory threadFactory, int ttlSeconds) {
+                return Schedulers.fromExecutorService(new ThreadPoolExecutor(threadCap, threadCap, ttlSeconds, TimeUnit.SECONDS, new LinkedBlockingDeque<>(1000), threadFactory));
+            }
+        });
+
+        Schedulers.boundedElastic(); // BoundedElasticScheduler: 最大线程数:10*cores, ttl:100000,
+        Schedulers.parallel(); // newParallel: 线程数: cores
+        Schedulers.single(); // newSingle: 线程数: 1
+    }
+
+    @Test
+    public void context() {
+        String key = "message";
+        Mono<String> r = Mono.just("Hello")
+                .flatMap( s -> Mono.subscriberContext().map( ctx -> s + " " + ctx.get(key)))
+                .subscriberContext(ctx -> ctx.put(key, "World")); // 放入Context
+
+        StepVerifier.create(r).expectNext("Hello World").verifyComplete();
+    }
 }
